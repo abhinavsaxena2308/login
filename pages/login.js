504b7bdb6f9a48a981073f39d31f8136941e2e35
abhinavsaxena2308 <124580551+abhinavsaxena2308@users.noreply.github.com>
2023-05-20 16:03:36 +0530

    first commit

---
+++ pages/login.js
@@ -0,0 +1,91 @@
+import Head from "next/head";
+import Link from "next/link";
+import { useState } from "react";
+import { signIn, signOut } from "next-auth/react";
+import { useFormik } from "formik";
+import login_validate from "../lib/validate";
+import { useRouter } from "next/router";
+
+export default function Login() {
+  const [show, setShow] = useState(false);
+  const router = useRouter();
+  // formik hook
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validate: login_validate,
+    onSubmit,
+  });
+
+  async function onSubmit(values) {
+    const status = await signIn("credentials", {
+      redirect: false,
+      email: values.email,
+      password: values.password,
+      callbackUrl: "/",
+    });
+
+    if (status.ok) router.push(status.url);
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Login</title>
+      </Head>
+
+      <div className="title">
+        <h1 className="text-white text-4xl font-bold py-4 text-center ">
+          Student Login
+        </h1>
+      </div>
+
+      <section className="w-3/4 mx-auto flex flex-col gap-10 py-4">
+        <form className="flex flex-col gap-5" onSubmit={formik.handleSubmit}>
+          <div
+            className={`${
+              formik.errors.email && formik.touched.email
+                ? "border-rose-600 "
+                : ""
+            }`}
+          >
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              className="input"
+              {...formik.getFieldProps("email")}
+            />
+          </div>
+
+          <div
+            className={`${
+              formik.errors.password && formik.touched.password
+                ? "border-rose-600"
+                : ""
+            }`}
+          >
+            <input
+              type={`${show ? "text" : "password"}`}
+              name="password"
+              placeholder="password"
+              className="input"
+              {...formik.getFieldProps("password")}
+            />
+          </div>
+          <div className="input-button">
+            <button type="submit" className="bg-orange-600 h-8 w-24 text-white">Login</button>
+          </div>
+        </form>
+        <p className="text-center text-gray-400 font-semibold">
+          don't have an account yet?{" "}
+          <Link href={"/register"}>
+            <a className="text-orange-900 font-semibold">Sign Up</a>
+          </Link>
+        </p>
+      </section>
+    </>
+  );
+}
---
+++ pages/register.js
@@ -0,0 +1,99 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import { useState } from 'react';
+import { useFormik } from 'formik';
+import { registerValidate } from '../lib/validate'
+import { useRouter } from 'next/router';
+
+export default function Register(){
+
+    const [show, setShow] = useState({ password: false, cpassword: false })
+    const router = useRouter()
+    const formik = useFormik({
+        initialValues: {
+            username : '',
+            email: '',
+            password: '',
+            cpassword: ''
+        },
+        validate: registerValidate,
+        onSubmit
+    })
+
+    async function onSubmit(values){
+        const options = {
+            method: "POST",
+            headers : { 'Content-Type': 'application/json'},
+            body: JSON.stringify(values)
+        }
+
+        await fetch('http://localhost:3000/api/auth/signup', options)
+            .then(res => res.json())
+            .then((data) => {
+                if(data) router.push('http://localhost:3000')
+            })
+    }
+
+    return (
+        <>
+        <Head>
+            <title>Register</title>
+        </Head>
+        <div className="title">
+            <h1 className="text-white text-4xl font-bold py-6 text-center ">
+                Student Register
+            </h1>
+        </div>
+
+        <section className='w-3/4 mx-auto flex flex-col gap-10'>
+            {/* form */}
+            <form className='flex flex-col gap-5' onSubmit={formik.handleSubmit}>
+                <div className={`${formik.errors.username && formik.touched.username ? 'border-rose-600' : ''}`}>
+                    <input 
+                    type="text"
+                    name='Username'
+                    placeholder='Username'
+                    className="input"
+                    {...formik.getFieldProps('username')}
+                    />
+                </div>
+                <div className={`${formik.errors.email && formik.touched.email ? 'border-rose-600' : ''}`}>
+                    <input 
+                    type="email"
+                    name='email'
+                    placeholder='Email'
+                    className="input"
+                    {...formik.getFieldProps('email')}
+                    />
+                </div>
+                <div className={`${formik.errors.password && formik.touched.password ? 'border-rose-600' : ''}`}>
+                    <input 
+                    type={`${show.password ? "text" : "password"}`}
+                    name='password'
+                    placeholder='password'
+                    className="input"
+                    {...formik.getFieldProps('password')}
+                    />
+                </div>
+                <div className={`${formik.errors.cpassword && formik.touched.cpassword ? 'border-rose-600' : ''}`}>
+                    <input 
+                    type={`${show.cpassword ? "text" : "password"}`}
+                    name='cpassword'
+                    placeholder='Confirm Password'
+                    className="input"
+                    {...formik.getFieldProps('cpassword')}
+                    />
+                </div>
+                <div className="input-button">
+                    <button type='submit' className="bg-orange-600 h-8 w-24 text-white">
+                        Sign Up
+                    </button>
+                </div>
+            </form>
+            <p className='text-center text-gray-400 '>
+                Have an account? <Link href={'/login'}><a className='text-orange-700'>Sign In</a></Link>
+            </p>
+        </section>
+        </>
+    )
+}
